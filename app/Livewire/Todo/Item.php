fe957bca2642701ed64d92414d543b5c0f8acdc2
Habib <habibpourmohammady@gmail.com>
2024-04-19 12:02:26 +0330

    The todo update section has been completed.

--- app/Livewire/Todo/Item.php
+++ app/Livewire/Todo/Item.php
@@ -3,8 +3,9 @@
 namespace App\Livewire\Todo;
 
 use App\Models\Todo;
-use Livewire\Attributes\On;
 use Livewire\Component;
+use Livewire\Attributes\On;
+use Livewire\Attributes\Validate;
 
 class Item extends Component
 {
@@ -14,6 +15,22 @@ class Item extends Component
      * @var \App\Models\Todo
      */
     public Todo $todo;
+
+
+    /**
+     * Indicates whether the status is being updated.
+     *
+     * @var bool
+     */
+    public bool $updateStatus = false;
+
+    /**
+     * The task input field.
+     *
+     * @var string
+     */
+    #[Validate("required|min:2|max:150")]
+    public string $task;
 
     /**
      * Change the completion status of a todo task.
@@ -28,6 +45,41 @@ class Item extends Component
         // Dispatch the "update-list" and "update-item" events to update the todo list
         $this->dispatch("update-list");
         $this->dispatch("update-item");
+    }
+
+
+    /**
+     * Toggles the update status.
+     *
+     * @return void
+     */
+    public function changeUpdateStatus()
+    {
+        $this->updateStatus = !$this->updateStatus;
+        $this->task = $this->todo->task;
+    }
+
+    /**
+     * Updates the task for the specified todo.
+     *
+     * @param  Todo  $todo The todo to be updated.
+     * @return void
+     */
+    public function update(Todo $todo)
+    {
+        // Validates the task input
+        $this->validateOnly("task");
+
+        // Updates the task for the todo
+        $todo->update([
+            "task" => $this->task
+        ]);
+
+        // Dispatches the update-item event
+        $this->dispatch("update-item");
+
+        // Resets the updateStatus and task properties
+        $this->reset("updateStatus", "task");
     }
 
     /**
--- resources/views/livewire/todo/item.blade.php
+++ resources/views/livewire/todo/item.blade.php
@@ -6,21 +6,36 @@
                     id="task-{{ $todo->id }}" type="checkbox"
                     class="w-6 h-6 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500">
             </section>
-            <label for="task-{{ $todo->id }}" class="ms-3">
-                <small
-                    class="font-medium {{ $todo->completion_status == 'completed' ? 'line-through text-green-500' : '' }}">
-                    {{ $todo->task }}
-                </small>
-                <br>
-                <small>{{ $todo->created_at }}</small>
-            </label>
+            @if (!$updateStatus)
+                <label for="task-{{ $todo->id }}" class="ms-3">
+                    <small
+                        class="font-medium {{ $todo->completion_status == 'completed' ? 'line-through text-green-500' : '' }}">
+                        {{ $todo->task }}
+                    </small>
+                    <br>
+                    <small>{{ $todo->created_at }}</small>
+                </label>
+            @else
+                <input type="text" wire:model="task"
+                    class="ms-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full px-2.5 font-sans"
+                    value="{{ $todo->task }}">
+                <button type="button" wire:click="update({{ $todo }})"
+                    class="ms-1 bg-gray-200 py-1 px-2 delay-75 transition-all hover:text-blue-700 rounded-md">
+                    <i class="fa fa-pencil-alt"></i>
+                </button>
+                <button wire:click="changeUpdateStatus" type="button"
+                    class="ms-1 bg-gray-200 py-1 px-2 delay-75 transition-all hover:text-red-700 rounded-md">
+                    <i class="fa fa-times-circle"></i>
+                </button>
+            @endif
         </section>
         <section>
-            <button type="button"
+            <button type="button" wire:click="changeUpdateStatus"
                 class="bg-gray-200 py-1 px-2 delay-75 transition-all hover:text-blue-700 rounded-md ms-1">
                 <i class="fa fa-pen"></i>
             </button>
-            <button wire:click="delete({{ $todo }})" type="button" class="bg-gray-200 py-1 px-2 delay-75 transition-all hover:text-red-700 rounded-md">
+            <button wire:click="delete({{ $todo }})" type="button"
+                class="bg-gray-200 py-1 px-2 delay-75 transition-all hover:text-red-700 rounded-md">
                 <i class="fa fa-trash"></i>
             </button>
         </section>
